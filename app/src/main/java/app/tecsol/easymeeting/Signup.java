e5f14fc47ed10cee94bf18bd2dc5ce8f8b472abb
ChHaseeb <chhaseebyounas1@gmail.com>
2020-06-04 22:01:44 +0500

    DataBAse added

--- app/src/main/java/app/tecsol/easymeeting/Signup.java
+++ app/src/main/java/app/tecsol/easymeeting/Signup.java
@@ -6,63 +6,74 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import app.tecsol.easymeeting.Models.UserModel;
+
 public class Signup extends AppCompatActivity {
 
-    private static final String TAG ="helo" ;
     Button btnsignup;
     EditText edtfullnamesignup,edtemailsignup,edtpasswordsignup;
-
-    // Write a message to the database
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("message");
-
+    DatabaseReference userRef;
+    FirebaseAuth mAuth;
+    String nameSTr, PasswordStr, EmailStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-
-        myRef.setValue("Hello, World!");
-
+        userRef=FirebaseDatabase.getInstance().getReference("User");
+        mAuth=FirebaseAuth.getInstance();
 
         btnsignup=findViewById(R.id.btnsignup);
         edtfullnamesignup=findViewById(R.id.edtfullnamesignup);
         edtemailsignup=findViewById(R.id.edtemailsignup);
         edtpasswordsignup=findViewById(R.id.edtfullnamesignup);
 
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
-
-
-
         btnsignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Signup.this, Login.class);
-                startActivity(i);
-                myRef.setValue("Hello, World!");
+                nameSTr=edtfullnamesignup.getText().toString();
+                PasswordStr=edtpasswordsignup.getText().toString();
+                EmailStr=edtemailsignup.getText().toString();
+                mAuth.signInWithEmailAndPassword(EmailStr, PasswordStr).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            String UserId=mAuth.getCurrentUser().getUid();
+                            UserModel model=new UserModel(nameSTr, EmailStr,PasswordStr, UserId);
+                            userRef.child(UserId).setValue(model).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()){
+                                        startActivity(new Intent(getBaseContext(), Agenda.class));
+                                    }
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(Signup.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        }
+                    }
+                });
+
             }
         });
 
